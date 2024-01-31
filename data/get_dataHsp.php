19c7f3887bac69157f958dec751ffe27036c85fb
Milena Neves <80131766+milenafbn@users.noreply.github.com>
2024-01-31 09:27:19 -0300

    feat adiciona nova coluna de dados

--- data/get_dataHsp.php
+++ data/get_dataHsp.php
@@ -14,15 +14,26 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Consulta SQL
-$sql = "SELECT HOSPITAIS, CNES, MUNICIPIOS, ENDERECO, COORDENADAS FROM tb_hsp";
-$result = $conn->query($sql);
+$idReg = "";
+
+$cl = ($idReg == "") ? "" : "AND mun_codreg = '".$idReg."' ";
+
+// Consulta SQL para obter dados dos hospitais
+$consultaHospitais = "SELECT HOSPITAIS, CNES, MUNICIPIOS, ENDERECO, COORDENADAS, mun_codreg  as 'COD_REG'
+                     FROM tb_hsp h, tb_municipios m
+                     WHERE h.CODMUN6 = m.mun_ibge6 ".$cl."";
+$resultadoHospitais = $conn->query($consultaHospitais);
+
+// Consulta SQL para obter códigos regionais distintos
+$consultaRegioes = "SELECT DISTINCT mun_codreg AS 'CÓDIGO REGIONAL' FROM tb_municipios";
+$resultadoRegioes = $conn->query($consultaRegioes);
 
 $markers = array();
-while ($row = $result->fetch_assoc()) {
-    //transforma string em array 
+
+while ($row = $resultadoHospitais->fetch_assoc()) {
+    // Transforma string em array 
     $row['COORDENADAS'] = json_decode($row['COORDENADAS'], true);
-        // Verifica se as coordenadas não são nulas
+    // Verifica se as coordenadas não são nulas
     if ($row['COORDENADAS'] !== null && count($row['COORDENADAS']) === 2) {
         $markers[] = $row;
     }
